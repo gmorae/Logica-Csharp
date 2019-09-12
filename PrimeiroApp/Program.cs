1640e040967c1c9a6033c7d3b6a930d25498cbea
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-12 12:24:28 -0300

    Estrutura base

---
+++ PrimeiroApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
