edac8cf1fb3f2863da1cde7ff72dc25cbd9c6894
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-12 14:18:12 -0300

    Programa que mostra a média de uma escola

--- PrimeiroApp/Program.cs
+++ PrimeiroApp/Program.cs
@@ -6,7 +6,18 @@ namespace PrimeiroApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1;
+            int n2;
+            int n3;
+            int soma;
+            Console.WriteLine("Digite uma nota: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a terceira nota: ");
+            n3 = int.Parse(Console.ReadLine());
+            soma = (n1 + n2 + n3) / 3;
+            Console.WriteLine("A média do aluno é = {0}", soma);
         }
     }
 }
