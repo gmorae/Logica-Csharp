159c89f06fbcbc940f2d168fb30cb2e779d9c07e
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-13 15:29:05 -0300

    Programa de cotacao de dola

---
+++ reais para dolar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace reais_para_dolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float reais, cotacao;
+
+            Console.WriteLine("Digite o valor em reais: ");
+            reais = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da cotação do dolar: ");
+            cotacao = float.Parse(Console.ReadLine());
+
+            float dolar = reais / cotacao;
+
+            Console.WriteLine($"O valor em dolares é = {dolar}");
+        }
+    }
+}
