fddace5bb5dbe95b4d83aa46464bcaffe7b1d6c3
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-13 14:29:52 -0300

    Programa que calcula o triplo de um numero qualquer

---
+++ triplo do numero/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace triplo_do_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n;
+           Console.WriteLine("Entre com um número");
+           n = int.Parse(Console.ReadLine());
+
+           int triplo = n * n* n;
+
+           Console.WriteLine($"O triplo de {n} é {triplo}");
+        }
+    }
+}
