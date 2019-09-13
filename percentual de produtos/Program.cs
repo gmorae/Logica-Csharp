31857cd9d4fdd752a47ccddede16aab58bd1284b
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-13 14:45:54 -0300

    Programa que le um número e uma porcentagem e acrecenta o valor no total

---
+++ percentual de produtos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace percentual_de_produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int porcentagem;
+            int valor;
+
+            Console.WriteLine("Digite o valor do produto: ");
+            valor = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da porcentagem: ");
+            porcentagem = int.Parse(Console.ReadLine());
+
+            int total1 = (valor * porcentagem)/100;
+            int total2 = total1 + valor;
+
+            Console.WriteLine($"O valor agora é = {total2}");
+        }
+    }
+}
