6cc8f6eb90321025b955cc412d0acddd3a802ff9
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-13 15:05:54 -0300

    Programa que calcula comissão

---
+++ Comissao de vendas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Comissao_de_vendas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float comissao, salario, vendas;
+
+            Console.WriteLine("Digite o quando você ganha de comissão: ");
+            comissao = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o seu salario: ");
+            salario = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o total de vendas que você teve no mês: ");
+            vendas = float.Parse(Console.ReadLine());
+
+            float totalVendas =  (vendas * comissao)/100;
+            float total = totalVendas + salario;
+
+            Console.WriteLine($"O seu salário mensal agora é = {total}");
+        }
+    }
+}
