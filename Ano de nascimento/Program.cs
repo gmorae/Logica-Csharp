9a4591b249752a95a4b2dd7ec3b2ab7259a6a6a1
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-13 14:22:16 -0300

    Programa que mostra sua idade e o total de semanas que você viveu

---
+++ Ano de nascimento/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ano_de_nascimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento, anoAtual;
+
+            Console.WriteLine("Digite seu ano de nascimento");
+            anoNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano atual");    
+            anoAtual = int.Parse(Console.ReadLine());
+
+            int idade = anoAtual - anoNascimento;
+            int meses = idade * 12;
+            int semana = meses / 4;
+            Console.WriteLine($"A sua idade é = {idade} anos");
+            Console.WriteLine($"E você viveu {semana} semanas");
+
+        }
+    }
+}
