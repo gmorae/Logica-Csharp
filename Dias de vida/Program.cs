fc1e5acd3d632b531724c900f5b44b8dbb57c52b
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-13 12:27:26 -0300

    Programa que mostra os meses, dias, anos e minutos que você viveu

---
+++ Dias de vida/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dias_de_vida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float idade; 
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            // Processamento
+            
+            float meses = idade * 12;
+            float dias = idade * 365;
+            float horas = dias * 24;
+            float minutos = horas * 60;
+
+            // Respostas
+
+            Console.WriteLine($"meses = {meses},\n dias = {dias},\n horas = {horas}\n minuto = {minutos}");
+        }
+    }
+}
