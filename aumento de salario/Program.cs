ef726802400d78d5de4eb103b7e61a27a8d6e417
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-16 13:15:19 -0300

    Aumento de salario de forma dinamica

---
+++ aumento de salario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aumento_de_salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salarioAtual;
+
+            Console.WriteLine("Digite seu salario atual: ");
+            salarioAtual = float.Parse(Console.ReadLine());
+
+            float reajuste = salarioAtual * 30 / 100;
+            float novoSalario = reajuste + salarioAtual;
+
+            if (salarioAtual > 990)
+            {
+                Console.WriteLine("Você não receberá o aumento");
+            }else{
+                Console.WriteLine($"O seu novo salario é = {novoSalario}");
+            }
+        }
+    }
+}
