a60ae8dc6c9f56db8abfce45674f383cc34c9531
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-16 14:25:12 -0300

    Faça um programa que receba a idade e mostre a categoria

---
+++ Categoria de nadadores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Categoria_de_nadadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade < 7)
+            {
+                Console.WriteLine("Categoria infantil A");
+            }
+            else if (idade <= 10)
+            {
+                Console.WriteLine("Catedoria infantil B");
+            }
+            else if (idade <= 13)
+            {
+                Console.WriteLine("Categoria Juvenil A");
+            }
+            else if (idade <= 17)
+            {
+                Console.WriteLine("Categoria Juvenil B");   
+            }
+            else if(idade > 18){
+                Console.WriteLine("Categoria adulto");
+            }
+        }
+    }
+}
