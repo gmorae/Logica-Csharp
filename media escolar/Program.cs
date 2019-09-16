bec48d63f304a089fcee32e42ec71fbac62c72a4
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-16 10:27:42 -0300

    Programa que calcula a média usuando comando de decisões (if e else)

---
+++ media escolar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace media_escolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2;
+            String nome;
+
+            Console.Write("Olá professor digite o nome do aluno: ");
+            nome = Console.ReadLine();
+            Console.Write($"Digite a nota do {nome}: ");
+            n1 = float.Parse(Console.ReadLine());
+            Console.Write($"{nome} digite outra nota: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            float media = (n1 + n2) / 2;
+
+            if (media >= 7)
+            {
+                Console.Write($"Professor, o {nome} foi aprovado");
+            }else{
+                Console.Write($"Professor, o {nome} foi reprovado");
+            }
+        }
+    }
+}
