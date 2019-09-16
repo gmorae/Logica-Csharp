4cfc52838a808fe2d4d53780b825b38be557b74e
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-16 09:41:57 -0300

    Programa que pede o salario e a divida e mostra o  resto do salario

---
+++ Conta de divida/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Conta_de_divida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario, contas;
+
+            Console.WriteLine("Digite o seu sálario");
+            salario = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da suas dividas");
+            contas = float.Parse(Console.ReadLine());
+
+            float total = salario - contas;
+
+            Console.WriteLine($"O seu sálario agora é de = {total} reais ");
+        }
+    }
+}
