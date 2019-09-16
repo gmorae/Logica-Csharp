dd7e13f723614f4992ca06f43e2c707322788625
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-16 10:03:54 -0300

    Programa que le o valor da passgem e o total de passageiros e mostra o total de reais

---
+++ Passagem do onibus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Passagem_do_onibus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float passagem;
+            int usuarios;
+            string motorista;
+            Console.WriteLine("Olá motorista, digite o seu nome: ");
+            motorista = Console.ReadLine();
+            Console.WriteLine($"Olá {motorista}, digite o valor atual da passagem: ");            
+            passagem = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Muito bem {motorista}, agora você deve digitar a quantidade de passageiros que tem no ônibus: ");
+            usuarios = int.Parse(Console.ReadLine());
+
+            float total = passagem * usuarios;
+            Console.WriteLine($"{motorista}, o total que você arrecadou nesta viagem foi: {total} reais");
+        }
+    }
+}
