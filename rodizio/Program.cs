5b2e4948928aecf3d3b8fc30762a974145d054bb
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-17 14:44:06 -0300

    Sistema de rodizio que verifica a placa e mostra o dia da semana que é proibida a circulação

---
+++ rodizio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace rodizio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String semana;
+            Console.WriteLine("Digite a sua placa: ");
+            String placa = Console.ReadLine();
+            int cont = placa.Length;
+            string final = placa.Substring(cont - 1);
+
+            if (final == "0" || final == "1")
+            {
+                semana = "Segunda-feira";
+
+            }else if (final == "2" || final == "3")
+            {
+                semana = "Terça-feira";
+
+            }else if (final == "4" || final == "5")
+            {
+                semana = "Quarata-feira";
+            }else if (final == "6" || final == "7")
+            {
+                semana = "Quinta-feira";
+
+            }else{
+
+                semana = "Sexta-feira";
+            }
+
+            Console.WriteLine($"O seu rodizio é na {semana}");
+
+
+        }
+    }
+}
