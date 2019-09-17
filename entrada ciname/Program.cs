af78b04efc25b583af70fa0e3ed46eb4a6e3df0b
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-17 11:41:43 -0300

    Sistema que verifica a idade do usuário e mostra se ele paga meia ou não

---
+++ entrada ciname/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace entrada_ciname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade < 18 || idade >= 60)
+            {
+                Console.WriteLine("Você irá pagar meia");
+            }else{
+                Console.WriteLine("Você irá pagar interia");
+            }
+        }
+    }
+}
