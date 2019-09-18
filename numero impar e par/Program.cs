d8d4d8162e53edabc5541e19998037b4d0258ad8
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-18 10:26:36 -0300

    Verificando dois números se é impar ou par e mostrando o maior número

--- numero impar e par/Program.cs
+++ numero impar e par/Program.cs
@@ -7,12 +7,26 @@ namespace numero_impar_e_par
         static void Main(string[] args)
         {
             Console.WriteLine("Digite um número: ");
-            int numero = int.Parse(Console.ReadLine());
-            
-            if( numero%2 != 1){
-                Console.WriteLine("O número é par");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número");
+            int n2 = int.Parse(Console.ReadLine());
+            if( n1%2 != 1){
+                Console.WriteLine($"O número {n1} é par \n");
             }else{
-                Console.WriteLine("O número é impar");
+                Console.WriteLine($"O número {n1} é impar \n");
+            }
+            if( n2%2 != 1){
+                Console.WriteLine($"O número {n2} é par \n");
+            }else{
+                Console.WriteLine($"O número {n2} é impar \n");
+            }
+
+            if (n1 > n2)
+            {
+                Console.WriteLine($"O número {n1} é maior \n");
+            }else
+            {
+                Console.WriteLine($"O número {n2} é maior \n");
             }
         }
     }
