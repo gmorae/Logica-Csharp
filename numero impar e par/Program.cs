524a87250eb022271879fff786bbd4a33a38e0df
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-18 09:48:49 -0300

    Programa que mostra se o número digitado é par ou impar

---
+++ numero impar e par/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace numero_impar_e_par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+            
+            if( numero%2 != 1){
+                Console.WriteLine("O número é par");
+            }else{
+                Console.WriteLine("O número é impar");
+            }
+        }
+    }
+}
