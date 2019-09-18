7927e99322f95e8698cc9efc0e8dffd75b09619f
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-18 15:52:38 -0300

    Tabuada com o camando while

---
+++ tabuada com while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tabuada_com_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número e eu direi a tabuada: ");
+            int tabuada = int.Parse(Console.ReadLine());
+            int i = 0;
+            Console.WriteLine("---------------------------------------");
+            while (i <= 10)
+            {
+                Console.WriteLine($"{tabuada} x {i} = {tabuada*i}");
+                i++;
+            }
+
+            Console.WriteLine("Fim");
+        }
+    }
+}
