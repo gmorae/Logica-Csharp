2e26761bdd4dcdb461e0a12be908b6456a02ec50
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-19 14:53:44 -0300

    Programa que armazena uma lista e depois mostra usando do while e array

---
+++ lista de frutas (array)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lista_de_frutas__array_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua lista de compra: \n");
+            
+            Console.WriteLine("Digite quantos produtos você vai comprar: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            String[] listas = new String[valor];
+            int n = 0;
+            do{
+                Console.WriteLine("Digite o seu produto: ");
+                listas[n] = Console.ReadLine();
+                Console.WriteLine("-----PRÓXIMO-----");
+                n++;
+            }while (n < valor);
+            n = 0;
+            do{
+                Console.WriteLine($"produto => {listas[n]}");
+                n++;
+            }
+            while (n < valor);
+        }
+    }
+}
