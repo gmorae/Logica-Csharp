28593757ff1c6352c66f86041f3200ce51715f50
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-19 15:54:30 -0300

    Validação com o do while

---
+++ validação com do while/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace validação_com_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validação com do while");
+
+            String nome;
+            do
+            {
+                Console.WriteLine("Digite seu nome: ");
+                nome = Console.ReadLine();
+            } while (nome.Length <= 1);
+        }
+    }
+}
