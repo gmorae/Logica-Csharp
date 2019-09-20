592e7ee1de9d138ef021058ae27080a09b3030db
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-20 11:47:55 -0300

    Sistema de verificação de email e senha com do while

---
+++ validacao de email com do while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace validacao_de_email_com_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email, senha;
+            do
+            {
+                Console.WriteLine("Digite seu email: ");
+                email = Console.ReadLine();
+            } while (!email.Contains("@") || !email.Contains("."));
+
+            do
+            {
+                Console.WriteLine("Digite sua senha: ");
+                senha = Console.ReadLine();   
+            } while (senha.Length < 8);
+
+        }
+    }
+}
