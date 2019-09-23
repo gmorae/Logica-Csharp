6e1ed734455f77bf039b3d5a1d7f7c627e9c8e33
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-23 09:54:46 -0300

    Programa que solicita a idade e mostra a quantidade de pessoas maiores e menores de 18 anos

---
+++ pessoas maiores de 18 anos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pessoas_maiores_de_18_anos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = 0, idade, i, menores = 0;
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                if (idade >= 18)
+                {
+                    maior++;
+                }else{
+                    menores++;
+                }
+            }
+            Console.WriteLine($"Quantidades de idades maiores de 18 = {maior} pessoas e menores de 18 = {menores} pessoas");
+            
+        }
+    }
+}
