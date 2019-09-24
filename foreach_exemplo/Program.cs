4cf7ed30b18a97dd9fea3050f2449882fab542e7
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-24 12:10:18 -0300

    Exemplo de foreach

---
+++ foreach_exemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace foreach_exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de foreach");
+
+            string[] nomes = { "", "Gabriel" };
+
+            foreach (var item in nomes)
+            {
+                if (!String.IsNullOrEmpty(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
