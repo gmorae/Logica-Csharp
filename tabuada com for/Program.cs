a5b601068f88d56100a8a85dcfba6479a02740a0
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-24 09:54:41 -0300

    Tabuada feita com o laço de repetição for

---
+++ tabuada com for/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tabuada_com_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero*i}");
+            }
+        }
+    }
+}
